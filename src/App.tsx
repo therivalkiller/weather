5b6b6ea01f544e91a6167039b300b00e91fde542
therivalkiller <97250364+therivalkiller@users.noreply.github.com>
2024-11-14 01:15:40 +0530

    New weather app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import { Button } from './components/ui/button';
+
+function App() {
+    return <div>Hello
+
+        <Button variant="destructive">Again heklo</Button>
+    </div>
+}
+export default App;
